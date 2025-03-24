6423fc6837f115cfc8c471611b9716372d004a9c
Clay Freeman <git@clayfreeman.com>
2025-03-23 20:44:01 -0500

    update scripts/setup-test-environment.sh

--- scripts/setup-test-environment.sh
+++ scripts/setup-test-environment.sh
@@ -5,7 +5,8 @@ PHP_VERSION='8.1'
 
 echo Install required packages ... >&2
 sudo apt-add-repository -y ppa:ondrej/php
-sudo apt-get -qq install curl p7zip-full php"$PHP_VERSION"-{cli,curl,mbstring,xdebug,xml,zip} unzip
+sudo apt-get -qq install --no-install-recommends --no-install-suggests \
+  curl p7zip-full php"$PHP_VERSION"-{cli,curl,mbstring,xdebug,xml,zip} unzip
 
 echo Set the default version of PHP ... >&2
 sudo update-alternatives --set php $(which php"$PHP_VERSION")
